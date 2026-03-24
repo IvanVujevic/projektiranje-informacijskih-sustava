8bedb865de8a943adfd16e9b5875483c8e8296c9
lzeljak <lovrezeljak123@gmail.com>
2026-03-24 19:17:25 +0100

    Update Program.cs
    dodan pozdrav

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
             Console.WriteLine("Hello, World!");
 
             Console.WriteLine("Hello world 2");
+            Console.WriteLine("Pozdrav Vegi!");
         }
     }
 }
